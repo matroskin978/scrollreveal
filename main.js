2ee9fd9e4f5613fc412f94d9a8c736b7d37655a8
Админ <matroskin8@i.ua>
2024-11-03 14:55:09 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+ScrollReveal().reveal('.reveal-left', {
+    origin: 'left',
+    distance: '200px',
+    duration: 1000,
+    delay: 0,
+    easing: 'ease-out',
+    reset: true
+});
+
+ScrollReveal().reveal('.reveal-top', {
+    origin: 'top',
+    distance: '200px',
+    duration: 1000,
+    delay: 0,
+    easing: 'ease-out',
+    reset: true
+});
+
+ScrollReveal().reveal('.reveal-bottom', {
+    origin: 'bottom',
+    distance: '200px',
+    duration: 1000,
+    delay: 0,
+    easing: 'ease-out',
+    reset: true
+});
+
+ScrollReveal().reveal('.reveal-right', {
+    origin: 'right',
+    distance: '200px',
+    duration: 1000,
+    delay: 0,
+    easing: 'ease-out',
+    reset: true
+});
+
+ScrollReveal().reveal('.reveal-rotate', {
+    origin: 'right',
+    distance: '200px',
+    duration: 1000,
+    delay: 500,
+    easing: 'ease-in-out',
+    reset: true,
+    rotate: {
+        x: 500,
+        y: 500,
+        z: 150
+    },
+    scale: .3
+});
+
+ScrollReveal().reveal('.reveal-fade', {
+    scale: 0.85,
+    duration: 1000,
+    delay: 0,
+    interval: 80,
+    reset: true
+});
+
+ScrollReveal().reveal('.reveal-fade2', {
+    scale: 0.85,
+    duration: 1000,
+    delay: 500,
+    interval: 80,
+    reset: true
+})
